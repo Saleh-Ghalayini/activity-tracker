b3f56ff0ebdfadcaf03b966f1f3b05b7e39bd59f
saleh <ghalayinisaleh9@gmail.com>
2025-04-01 01:20:27 +0300

    initiated the basic structure of the frontend

---
+++ activity-frontend/src/pages/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <h1>Activity Dashboard</h1>
+    );
+};
+
+export default Dashboard;
---
+++ activity-frontend/src/pages/Upload/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+import React from "react";
+
+const Upload = () => {
+    return (
+        <h1>Upload CSV File</h1>
+    );
+};
+
+export default Upload;
