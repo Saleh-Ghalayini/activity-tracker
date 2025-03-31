0fff7b1d846bea578aa46fea4fd1d5eeaf6b7d6f
saleh <ghalayinisaleh9@gmail.com>
2025-04-01 01:49:18 +0300

    initiated the drop area

--- activity-frontend/src/pages/Upload/index.jsx
+++ activity-frontend/src/pages/Upload/index.jsx
@@ -1,9 +1,35 @@
 import './style.css';
-import React from "react";
+import '../../index.css'
+import { React, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 const Upload = () => {
+
+    const [file, setFile] = useState(null);
+
+    // Storing the first file being dropped
+    const onDrop = (acceptedFiles) => {
+        setFile(acceptedFiles[0]);
+    };
+
+    // Handling the onDrop event while keeping the functionality of the hidden file input 
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        accept: '.csv' // Allowing only CSV files
+    }); 
+
     return (
-        <h1>Upload CSV File</h1>
+        <div>
+            <h1>Upload CSV file</h1>
+            <div 
+                className="pointer drop-box text-center" 
+                {...getRootProps()}
+            >
+                <input {...getInputProps()} />
+                <p>Drag & drop a CSV file here, or click to select one</p>
+            </div>
+            {file && <p>Selected file: {file.name}</p>}
+        </div>
     );
 };
 
